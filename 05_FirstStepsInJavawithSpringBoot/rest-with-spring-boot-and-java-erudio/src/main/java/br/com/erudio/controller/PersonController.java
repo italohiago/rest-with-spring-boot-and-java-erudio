eaac868cf5050cd1b2be65de39f8934f84b346a6
Italo Hiago <italohiagolopes@outlook.com>
2023-11-24 19:06:30 -0300

    Final 07-08-09 classes

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -0,0 +1,55 @@
+package br.com.erudio.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import br.com.erudio.service.PersonServices;
+import br.com.erudio.vo.v1.PersonVO;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices personService;
+	
+	@GetMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById (
+			@PathVariable(value = "id") Long id
+			) throws Exception {
+				return personService.findById(id);		
+	}
+	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll (
+			) throws Exception {
+				return personService.findAll();		
+	}
+	
+	@PostMapping( produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create (
+			@RequestBody PersonVO person
+			) throws Exception {
+				return personService.create(person);
+	}
+	
+	@PutMapping( produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update (
+			@RequestBody PersonVO person
+			) throws Exception {
+				return personService.update(person);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete (
+			@PathVariable(value = "id") Long id
+			) throws Exception {
+				personService.delete(id);
+				return ResponseEntity.noContent().build();
+	}
+	
+	
+}
